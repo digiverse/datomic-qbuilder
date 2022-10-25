4e22042bfbe21a4af911d741bc302afa100ec4a8
Kristjan Cocev <kris@digiverse.si>
2022-10-25 13:51:36 +0200

    fix startup error:
    - undertow reported a session error upon first run (had to refresh the browser)

--- src/clj/datomic_qbuilder/middleware.clj
+++ src/clj/datomic_qbuilder/middleware.clj
@@ -2,16 +2,13 @@
   (:require
     [datomic-qbuilder.env :refer [defaults]]
     [datomic-qbuilder.config :refer [env]]
-    [ring.middleware.flash :refer [wrap-flash]]
-    [ring.adapter.undertow.middleware.session :refer [wrap-session]]
+    [ring-ttl-session.core :refer [ttl-memory-store]]
     [ring.middleware.defaults :refer [site-defaults wrap-defaults]])
   )
 
 (defn wrap-base [handler]
   (-> ((:middleware defaults) handler)
-      wrap-flash
-      (wrap-session {:cookie-attrs {:http-only true}})
       (wrap-defaults
         (-> site-defaults
             (assoc-in [:security :anti-forgery] false)
-            (dissoc :session)))))
+            (assoc-in  [:session :store] (ttl-memory-store (* 60 30)))))))
